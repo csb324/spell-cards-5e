9f1173af7288ce4312f1a72f8ebda89eadfe28c1
Clara <clara@freewill.com>
2022-11-19 16:54:17 -0500

    Format on save huh

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import EditCard from "../components/EditCard";
 function App() {
   const [activeCard, setActiveCard] = useState(-1);
   const [cardsData, setCardsData] = useState([SpellApiService.get('')]);
+
   const selectFunction = (index:number) => {
     return () => {
       setActiveCard(index);
@@ -28,7 +29,6 @@ function App() {
 
 
   if(activeCard === -1) {
-    
     const cards = cardsData.map((c, index) => (
       <Card key={c.name} spell={c} select={selectFunction(index)} isActive={ index === activeCard }/>
     ));
@@ -41,8 +41,8 @@ function App() {
           { cards }
         </div>
       </div>
+    )
 
-    );
   } else {
     const c = cardsData[activeCard];
 
